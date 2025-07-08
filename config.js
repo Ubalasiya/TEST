1094d74898f804f66a89f9074c0637ff08bdc3e7
𝗖𝗛𝗔𝗠𝗜𝗬𝗔𝗛𝗛...! <skylifecommunity0@gmail.com>
2025-07-08 09:46:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "CHAMI-MD=zIEEFAZT#rGUvVVTbqwyV08UBP-A9SuvsloI2unuEBFMDhDvZWIs", // session id
+SESSION_ID: process.env.SESSION_ID || "zIEEFAZT#rGUvVVTbqwyV08UBP-A9SuvsloI2unuEBFMDhDvZWIs", // session id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", // true,false
 ANTI_DELETE: process.env.ANTI_DELETE || "true", // true,false
 MODE: process.env.MODE || "public", // public,private,groups,inbox
